07f78e5c62d65140ad59ba3c54f170e2b9ff733e
Alex Burrows <burrows.alexj@gmail.com>
2024-12-01 12:09:16 -0500

    Generate array of Die components to render

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import Die from "./components/Die"
+import { useState } from "react"
 
 export default function App(){
+  const [dieVals, setDieVals] = useState(generateAllNewDice())
+
+  function generateAllNewDice(){
+    const res = []
+    for (let i = 0; i < 10; i++){
+      res.push(Math.ceil(Math.random() * 6))
+    }
+    return res
+  }
+
+  const dieComponentsArray = dieVals.map(val => <Die value={val} />)
+
+  console.log(dieComponentsArray)
   return(
     <main className="game-container">
       <div className="dice-container">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+        {dieComponentsArray}
       </div>
 
     </main>
